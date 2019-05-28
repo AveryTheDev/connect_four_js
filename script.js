5969c75959838c41fd953785049c0ad2a6d8cb9c
abrown945 <abrown945@gmail.com>
2019-05-27 20:44:13 -0400

    Fixed mapping of event value

--- script.js
+++ script.js
@@ -1,70 +1,155 @@
-let gameMatrix = new Array(6).fill([new Array(7).fill(0)]);
-let playerOneSlots = new Array(6).fill([new Array(7).fill(0)]);
-let playerTwoSlots = new Array(6).fill([new Array(7).fill(0)]);
+let gameMatrix = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]; 
+
+let playerOneSlots = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]; 
+
+let playerTwoSlots = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]; 
 
 let playerTurn = 1;
 let win = false;
 
 const slotSelected = (event) => {
-    let columnNumber = (event.target.id);
+    debugger;
+    let columnNumber = (event);
     columnNumber--;
-    // let slot;
+    let slot;
 
-    for(i = 0; i < 6; i++) {
-        if(array[i] === columnNumber) {
-            for(j = 0; j < 7; j++) {
-              if( array[i][j] === 0) {
-                  gameMatrix[i][j] = turn % 2 === 1 ? 1 : 2;
+    for(let i = 0; i < 6; i++) {
+        if(i === columnNumber) {
+            for(let j = 0; j < 7; j++) {
+              if(gameMatrix[i][j] === 0) {
+                  gameMatrix[i][j] = playerTurn % 2 === 1 ? 1 : 2;
+                  console.log(gameMatrix[i][j]);
                    slot = j;
-                  if (turn % 2 === 1) {
+                  if (playerTurn % 2 === 1) {
                       playerOneSlots[i][j] = 1;
+                      console.log(playerOneSlots[i][j]);
                   }
-                  if (turn % 2 === 0) {
-                      playerTwoSlots[i][j] = 2;
+                  else if (playerTurn % 2 === 0) {
+                      playerTwoSlots[i][j] = 2;                    
                   }
               }
+              break;
             }
         }
-
     }
-    
-    // let column = document.getElementById(event.target.id);
-    // let row = column.getElementById(slot);
 
-    // if(turn % 2 === 1) {
-    //     class = "playerOne";
-    // }
-    // if(turn % 2 === 0) {
-    //     class = "playerTwo";
-    // }
+    event = event.toString();
+    slot = slot.toString();
+    let location = event + "-" + slot;
+    let row = document.getElementById(location);
 
-    turn++;
+    if(playerTurn % 2 === 1) {
+        row.className += "playerOne";
+    }
+    if(playerTurn % 2 === 0) {
+        row.className += "playerTwo";
+    }
+
+    playerTurn++;
 }
 
 const gameStatusChange = () => {
-    if (turn % 2 === 1 && !win ) {
-        "Player One: Please Select a Column"
+    debugger;
+    let status = document.getElementsByClassName("subtitle").textContent;
+
+    if (playerTurn % 2 === 1 && !win ) {
+        status = "Player One: Please Select a Column"
     }
-    if (turn % 2 === 1 && win ) {
-        "Player One is the Winner!"
+    if (playerTurn % 2 === 1 && win ) {
+        status = "Player One is the Winner!"
     }
-    if (turn % 2 === 0 && !win ) {
-        "Player Two: Please Select a Column"
+    if (playerTurn % 2 === 0 && !win ) {
+        status = "Player Two: Please Select a Column"
     }
-    if (turn % 2 === 0 && win ) {
-        "Player Two is the Winner!"
+    if (playerTurn % 2 === 0 && win ) {
+        status = "Player Two is the Winner!"
     }
-    if (turn === 42 && !win) {
-        "Tie"
+    if (playerTurn === 42 && !win) {
+        status = "Tie"
     }
+
+    return status;
 }
 
-const ResetGame = () => {
-    gameMatrix.fill(0);
-    playerOneSlots.fill(0);
-    playerTwoSlots.fill(0);
+const resetGame = () => {
+    debugger;
+    gameMatrix = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+    ];
 
-    turn = 1;
+    playerOneSlots = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+    ];
+
+    playerTwoSlots = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+    ]; 
+
+    playerTurn = 1;
 }
 
 const resetButton = document.getElementsByClassName("button");
+const row = document.getElementsByTagName("ul");
+
+    row[0].addEventListener("click", (event) => {
+        slotSelected(event.currentTarget.id);
+        gameStatusChange();
+    })
+    row[1].addEventListener("click", (event) => {
+        slotSelected(event.currentTarget.id);
+        gameStatusChange();
+    })
+    row[2].addEventListener("click", (event) => {
+        slotSelected(event.currentTarget.id);
+        gameStatusChange();
+    })
+    row[3].addEventListener("click", (event) => {
+        slotSelected(event.currentTarget.id);
+        gameStatusChange();
+    })
+    row[4].addEventListener("click", (event) => {
+        slotSelected(event.currentTarget.id);
+        gameStatusChange();
+    })
+    row[5].addEventListener("click", (event) => {
+        slotSelected(event.currentTarget.id);
+        gameStatusChange();
+    })
+
+// resetButton.addEventListener("click", resetGame);
